da0439638a754e640aee03273a04d90aefab516e
Peter Stovka <stovka.peter@gmail.com>
2022-08-28 18:58:11 +0200

    init

---
+++ src/Providers/PanelsServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Insight\Panels\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class PanelsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
